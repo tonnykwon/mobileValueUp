513627b5ca0cd773da050c1eaa5f37fdba54aef6
EunjiLim <cucumbers816@gmail.com>
2015-06-25 15:36:22 +0900

    login activity

---
+++ Sample1_1/src/com/example/sample1_1/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sample1_1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+   
+    public void onLoginClicked(View v){
+    	Intent intent = new Intent(getApplicationContext(), friendListActivity.class);
+    	startActivity(intent);
+    }
+    
+    public void onSignInClicked(View v){
+    	Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"));
+    	startActivity(myIntent);
+    }
+}
---
+++ Sample1_1/src/com/example/sample1_1/friendListActivity.java
@@ -0,0 +1,15 @@
+package com.example.sample1_1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class friendListActivity extends ActionBarActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_friendlist);
+	}
+	
+	
+}
